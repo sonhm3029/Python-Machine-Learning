671ab579d4c8ccd3b233db8e134ce4c2fb505d0a
Son Hoang <hoangminhson29102001@gmail.com>
2022-10-05 00:25:10 +0700

    Thêm inceptionresnetv2 example with brain tumor

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-import time
-import sys
+from keras.applications import InceptionResNetV2
 
 
-toolbar_width = 40
-sys.stdout.write("[%s]" % (" " * toolbar_width))
-sys.stdout.flush()
-sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+model = InceptionResNetV2(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=1000,
+    classifier_activation="softmax",
+)
 
-for i in range(toolbar_width):
-    time.sleep(0.1) # do real work here
-    # update the bar
-    sys.stdout.write("-")
-    sys.stdout.flush()
 
-sys.stdout.write("]\n") # this ends the progress bar
+print(model.summary())
